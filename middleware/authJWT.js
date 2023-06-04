cb75add039aa82a0ab528963fb91835e285a8e4f
yjw0514 <jwjwhappy92@gmail.com>
2023-06-04 23:15:59 +0900

    feat. authJWT 에러 핸들링

--- middleware/authJWT.js
+++ middleware/authJWT.js
@@ -3,15 +3,18 @@ const jwt = require('jsonwebtoken');
 //token verify 체크
 const authJWT = (req, res, next) => {
   // header의 토큰 가져오기
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split('Bearer ')[1];
-
-  if (!token) return res.status(400);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) res.status(401).json({ message: 'token expired.' });
-    req.user = user;
-    next();
-  });
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split('Bearer ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        res.status(401).json({ message: 'token expired' });
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: '권한이 없습니다.' });
+  }
 };
 
 module.exports = authJWT;
